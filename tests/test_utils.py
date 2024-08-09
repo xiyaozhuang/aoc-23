77836d8fa26e247a48d9e9039e4ada52c63496a0
xiyaozhuang <114228792+xiyaozhuang@users.noreply.github.com>
2024-08-09 18:06:34 +0100

    Added tests for main script utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,27 @@
-from aoc23.utils import find_all
+import pytest
+
+from aoc23.utils import find_all, get_modules, get_solutions, load_data
+
+
+def test_load_data():
+    expected = {"hello": ["world"]}
+    output = load_data("data/dummy")
+
+    assert expected == output
+
+
+def test_get_modules():
+    expected = [pytest.fixture]
+    output = get_modules(pytest, "fixture", test=False)
+
+    assert output == expected
+
+
+def test_get_solutions():
+    expected = {"day_1": (55108, 56324)}
+    output = get_solutions(load_data("data/problems"), test=True)
+
+    assert output == expected
 
 
 def test_findall():
