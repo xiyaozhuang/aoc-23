28c1037ba2094b860030d1b2d1b95d6d40b43149
xiyaozhuang <114228792+xiyaozhuang@users.noreply.github.com>
2024-08-11 18:17:26 +0100

    Added test for part 2

--- tests/test_day_2.py
+++ tests/test_day_2.py
@@ -1,4 +1,4 @@
-from aoc23.day_2 import part_1
+from aoc23.day_2 import part_1, part_2
 
 
 def test_part_1():
@@ -7,5 +7,15 @@ def test_part_1():
 
     expected = 8
     output = part_1(data)
+
+    assert output == expected
+
+
+def test_part_2():
+    with open("data/tests/day_2b.txt") as file:
+        data = file.readlines()
+
+    expected = 2286
+    output = part_2(data)
 
     assert output == expected
